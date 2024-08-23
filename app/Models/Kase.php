5c01e471b01d867c15e48030db50735278e83333
Abdrahman Moussa <maloom1414@gmail.com>
2024-08-23 22:11:57 +0300

    create controllers and some views

--- app/Models/Kase.php
+++ app/Models/Kase.php
@@ -7,5 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kase extends Model
 {
-    use HasFactory;
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
 }
